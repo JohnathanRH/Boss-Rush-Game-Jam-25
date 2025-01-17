c7c53dbc82e5d715f55657c3a762d1e617ffa5ff
JohnathanRH <144834953+JohnathanRH@users.noreply.github.com>
2025-01-17 11:06:16 +0800

    Dual grid system
    fuck you windows

---
+++ spin/script/test_map.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+var tileChart = {
+	["Dirt","Dirt", "Dirt", "Grass"] : Vector2i(0,0),
+	["Grass","Grass", "Dirt", "Dirt"] : Vector2i(4,0),
+	["Grass","Dirt", "Grass", "Grass"] : Vector2i(8,0),
+	["Grass","Dirt", "Dirt", "Grass"] : Vector2i(12,0),
+	["Grass","Dirt", "Grass", "Dirt"] : Vector2i(0,4),
+	["Grass","Grass", "Dirt", "Grass"] : Vector2i(4,4),
+	["Grass","Grass", "Grass", "Grass"] : Vector2i(8,4),
+	["Dirt","Grass", "Grass", "Grass"] : Vector2i(12,4),
+	["Dirt","Grass", "Dirt", "Dirt"] : Vector2i(0,8),
+	["Dirt", "Grass", "Grass", "Dirt"] : Vector2i(4,8),
+	["Grass", "Grass", "Grass", "Dirt"] : Vector2i(8,8),
+	["Dirt", "Dirt", "Grass", "Grass"] : Vector2i(12,8),
+	["Dirt", "Dirt", "Dirt", "Dirt"] : Vector2i(0,12),
+	["Grass", "Dirt", "Dirt", "Dirt"] : Vector2i(4,12),
+	["Dirt", "Grass", "Dirt", "Grass"] : Vector2i(8,12),
+	["Dirt", "Dirt", "Grass", "Dirt"] : Vector2i(12,12),
+}
+
+func _ready() -> void:
+	for i in $Display.get_used_cells():
+		$Display.set_cell(i, 1,tileChart[[getTileType(i), getTileType(i+Vector2i(0,-1)), getTileType(i + Vector2i(-1,-1)), getTileType(i + Vector2i(-1,0))]])
+	#var current_cell = Vector2i(2,-1)
+	#getTileType(current_cell)
+	#getTileType(current_cell + Vector2i(0,-1))
+	#getTileType(current_cell + Vector2i(-1,-1))
+	#getTileType(current_cell + Vector2i(-1,0))
+
+func getTileType(coord : Vector2i):
+	var tileAtlas : Vector2i = $GroundData.get_cell_atlas_coords(coord)
+	if tileAtlas == Vector2i(8,4):
+		return "Grass"
+	else:
+		return "Dirt"
